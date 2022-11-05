1c3438609ec32ef91021d1f640b67d901e7d969b
Arham-Ahmed-Ansari <arhamahmedansari92@gmail.com>
2022-11-05 08:34:25 +0500

    Assignment done

---
+++ main.dart
@@ -0,0 +1,48 @@
+void main(List<String> args) {
+  // Question no 1:
+  // List<String> nameList = [
+  //   "Bilal",
+  //   "Bilal",
+  //   "Bilal",
+  //   "Owais",
+  //   "Owais",
+  //   "Owais"
+  // ];
+  // List<String> newList = nameList.toSet().toList();
+  // print(newList);
+
+  // Question no 2:
+
+  // var a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100];
+  // var b = a.where((element) => element.isEven).toList();
+  // print(b);
+
+  // Question no 2:
+
+  // for (var i = 0; i <= 15; i++) {
+  //   print("7 X $i = ${7 * i}");
+  // }
+
+  // Question no 3:
+
+  // var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+  // for (var i = 0; i <= 4; i++) {
+  //   print(fruits);
+  // }
+
+  // Question no 4:
+
+  // for (var i = 0; i <= 100; i++) {
+  //   print("${5 * i}");
+  // }
+
+  // Question no 5:
+
+  // num temperature = 40.6;
+  // num ferenhite = (temperature * 1.8) + 32;
+  // num celsius = (temperature - 32) * 0.5556;
+  // print("The temperature in ferenhite is: $celsius");
+  // print("The temperature in celsius is: $ferenhite");
+
+  // Question no 6:
+}
